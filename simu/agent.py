01482d976548a40891a266aa591abe152330da4f
Suhail-Ka-XPS-UWindsor <bains.singh.suhail@gmail.com>
2021-10-06 20:32:10 +0530

    Added testModel to Sim

--- simu/agent.py
+++ simu/agent.py
@@ -7,14 +7,16 @@
 # All the above things combined after 2 years to get the satisfaction. 
 
 
-from mesa import Agent
+from mesa import Agent,Model
 from mesa.datacollection import DataCollector
+from mesa.space import MultiGrid
 from mesa.space import Grid
 from mesa.time import RandomActivation
 
 class Student_Model(Agent):
     satisfaction_level_overall = 0
-    def __init__(self, arg_marks, gender, visa, dept):
+    def __init__(self, arg_marks, gender, visa, dept, unique_id, model):
+        super().__init__(unique_id, model)
         self.satisfaction = 0
         self.currentmarks = arg_marks
         self.gender = gender
@@ -23,33 +25,57 @@ class Student_Model(Agent):
         self.workload = 0.00
     
     def move(self):
-        # possible_steps = self.model.grid.get_neighborhood(
-        #     self.pos, moore=True, include_center=False
-        # )
-        # new_position = random.choice(1)
-        # self.model.grid.move_agent(self, new_position)
-        print ("in Move")
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+        print ("Going to this position: ", new_position)
     
     def step(self, workload):
         self.move()
-        student_group = [{"currentmarks": 6}]
-        # student_group = self.model.grid.get_cell_list_contents([self.pos])
+        # student_group = [{"currentmarks": 6}]
+        student_group = self.model.grid.get_cell_list_contents([self.pos])
         cummulative = 0.00
         for other_students in student_group:
-            # print ()
-            cummulative += other_students["currentmarks"]
+            # print (other_students.currentmarks)
+            cummulative += other_students.currentmarks
         cummulative = cummulative/len(student_group)
         self.currentmarks = (0.7*cummulative) + (0.3*self.currentmarks)
         self.workload += workload
+        print("Worload changed to: ", self.workload)
+        print("Marks changed to: ", self.currentmarks)
 
     def get_current_satisfaction(self):
         self.satisfaction = 0.2*(self.currentmarks) - 0.4*(self.workload) + 0.3*(self.gender) + 0.5*(self.visa) + 0.2*(self.dept)
-        print (self.satisfaction)
+        print ("Final Satisfaction: ", self.satisfaction)
 
     def end_of_life(self):
         self.satisfaction_level_overall = self.satisfaction
 
-test = Student_Model(0,0,0,0)
-test.move()
-test.step(5)
-test.get_current_satisfaction()
+class testModel(Model):
+    """A model with some number of agents."""
+    def __init__(self, N, width, height):
+        self.num_agents = N
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(10, 10, True)
+        # Create agents
+        for i in range(self.num_agents):
+            avg_marks = self.random.randrange(10)
+            gender = self.random.randrange(10)
+            visa = self.random.randrange(10)
+            dept = self.random.randrange(10)
+            workload = self.random.randrange(10)
+            a = Student_Model(avg_marks, gender, visa, dept,i, self)
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+            a.step(workload)
+            
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
+
+empty_model = testModel(100,100,100)
+empty_model.step()
